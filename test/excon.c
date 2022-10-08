91ef8164c7da1063d3b03b6048d0f189230adca4
Marshall Lochbaum <mwlochbaum@gmail.com>
2022-10-08 12:24:00 -0400

    Name suffixes for top-level constants

--- test/excon.c
+++ test/excon.c
@@ -1,6 +1,6 @@
-static int32_t si_c0[] = {((int32_t)11ll),((int32_t)10ll),((int32_t)9ll)};
+static int32_t si_c0_c[] = {((int32_t)11ll),((int32_t)10ll),((int32_t)9ll)};
 
 int16_t const num = ((int16_t)12ll);
 
-int32_t* const arr = si_c0;
+int32_t* const arr = si_c0_c;
 
--- test/fnarr.c
+++ test/fnarr.c
@@ -1,7 +1,7 @@
 static uint32_t si_f0_fn_0(uint32_t v0, uint32_t v1);
 static uint32_t si_f1_fn_1(uint32_t v0, uint32_t v1);
 
-static uint32_t (*si_c0[])(uint32_t,uint32_t) = {si_f0_fn_0,si_f1_fn_1};
+static uint32_t (*si_c0_fns[])(uint32_t,uint32_t) = {si_f0_fn_0,si_f1_fn_1};
 
 static uint32_t si_f0_fn_0(uint32_t v0, uint32_t v1) {
   return v0;
@@ -12,10 +12,10 @@ static uint32_t si_f1_fn_1(uint32_t v0, uint32_t v1) {
 }
 
 static uint32_t si_f2_sfn(bool v0, uint32_t v1, uint32_t v2) {
-  uint32_t (*v3)(uint32_t,uint32_t) = si_c0[v0];
+  uint32_t (*v3)(uint32_t,uint32_t) = si_c0_fns[v0];
   uint32_t v4 = v3(v1, v2);
   return v4;
 }
 
-uint32_t (**const fn_arr)(uint32_t,uint32_t) = si_c0;
+uint32_t (**const fn_arr)(uint32_t,uint32_t) = si_c0_fns;
 
