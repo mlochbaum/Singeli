eaba9354d6da9f7137855a8b2ecf53ac5c4682c9
Marshall Lochbaum <mwlochbaum@gmail.com>
2023-02-26 15:40:03 -0500

    Add test/alias.c

---
+++ test/alias.c
@@ -0,0 +1,18 @@
+static int32_t si_f0_f(int32_t v0_x) {
+  int32_t v1 = change(v0_x);
+  int32_t v2_a = v1;
+  int32_t v3_b = v0_x;
+  v2_a = ((int32_t)4ll);
+  v3_b = v1;
+  v0_x = v2_a;
+  return v3_b;
+}
+
+static int32_t* si_f1_g() {
+  int32_t v0_b = ((int32_t)5ll);
+  int32_t v1_c = v0_b;
+  v1_c = ((int32_t)6ll);
+  int32_t v2_a[] = {v0_b,v0_b};
+  return v2_a;
+}
+
